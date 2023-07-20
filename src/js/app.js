95484331e5aad5db67043d09c47146c694e56237
Your Name <you@example.com>
2023-07-20 09:39:55 -0400

    first commit

--- src/js/app.js
+++ src/js/app.js
@@ -31,19 +31,18 @@ function render(variables = {}) {
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-        </div>
-    `;
+  ${cover}
+  <img src="${variables.avatarURL}" class="photo" />
+  <h1>${variables.name} ${variables.lastname}</h1>
+  <h2>${variables.role}</h2>
+  <h3>${variables.city}, ${variables.country}</h3>
+  <ul class="${variables.socialMediaPosition}">
+    <li><a href="https://twitter.com/${variables.twitter}"><i class="fa-brands fa-twitter"></i></a></li>
+    <li><a href="https://github.com/${variables.github}"><i class="fa-brands fa-github"></i></a></li>
+    <li><a href="https://linkedin.com/${variables.linkedin}"><i class="fa-brands fa-linkedin"></i></a></li>
+    <li><a href="https://instagram.com/${variables.instagram}"><i class="fa-brands fa-instagram"></i></a></li>
+  </ul>
+</div>`;
 }
 
 /**
@@ -56,7 +55,8 @@ window.onload = function() {
     // this is the url of the image that will used as background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://scontent.fccp3-1.fna.fbcdn.net/v/t39.30808-6/272172023_2709056532573893_1615818109764331483_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=174925&_nc_ohc=qETjQbmN6M4AX8KxHoq&_nc_ht=scontent.fccp3-1.fna&oh=00_AfCFMTzg-BF_Chfqry-7cDMjV3YJzWCYuNgHAm-O52h5xA&oe=64676129",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
